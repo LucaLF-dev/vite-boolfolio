96f57e40ebf7ffdb60d834bdb029edb4da235d03
LucaLF-dev <139238422+LucaLF-dev@users.noreply.github.com>
2024-01-15 18:06:42 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  import ProjectIndex from './components/pages/projects/Index.vue';
+  export default {
+    components: {
+      ProjectIndex
+    }
+
+  }
+</script>
+
+<template>
+  <ProjectIndex/>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card card-project">
+      <h1>{{project.title}}</h1>
+      <p>{{ project.description }}</p>
+     
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      project: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+    .card.card-project {
+      padding: 40px;
+      border-radius: 12px;
+      background: #dedede;
+    }
+  </style>
